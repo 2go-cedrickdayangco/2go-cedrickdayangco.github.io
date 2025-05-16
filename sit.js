8f1daf27e299035437da21638dfef37eea89f4cf
cedrick <cedrickdayangco@gmail.com>
2025-05-16 09:55:12 +0800

    update

---
+++ sit.js
@@ -0,0 +1,122 @@
+{
+    "tracking_no": "AA6010030",
+    "pickup_type": "Drop Off",
+    "pickup_date": "2022-02-10",
+    "dest_system": "qair",
+    "billtosite_account": "20001414-50005046",
+    "qair_account_number": "2020000370",
+    "transaction_type": "PX",
+    "cust_short_name": "2020000370",
+    "store_id": "1013246",
+    "store_name": "2GO MNL - SM PASIG - FNP",
+    "store_type": "2GO Store Agent",
+    "booking_no": "",
+    "paymode": "Prepaid",
+    "service_type": "AD",
+    "shipper_billtosite_account": "20001414-50005046",
+    "shipper_name": "ADIDAS OUTLET CONSUMER PUP",
+    "shipper_contact": "09999999912",
+    "shipper_address": "14F FORT LEGEND TOWER, 3RD AVE COR 31ST BGC",
+    "ultimate_shipper": "",
+    "ultimate_shipper_contact": "09999999912",
+    "ultimate_shipper_address": "",
+    "ultimate_shipper_address_province": "",
+    "ultimate_shipper_address_city": "",
+    "ultimate_shipper_address_brgy": "",
+    "ultimate_shipper_address_street": "",
+    "ultimate_shipper_area_code": "1800",
+    "ultimate_shipper_zip": "",
+    "origin_port": "MNL",
+    "origin_area_code": "1400",
+    "origin_landmark": "ELEGANT PROJECT 8 QUEZON CITY",
+    "cnee_billtosite_account": "",
+    "dest_port": "MNL",
+    "cnee_name": "qaf013",
+    "cnee_contact": "09999999992",
+    "cnee_address": "test address01",
+    "cnee_address_province": "",
+    "cnee_address_city": "PAMPANGA",
+    "cnee_address_brgy": "test01",
+    "cnee_address_street": "",
+    "dest_area_code": "1400",
+    "ultimate_cnee": "",
+    "dest_landmark": "",
+    "destination_landmark": "",
+    "cnee_careoff_store": "",
+    "dest_store_port": "",
+    "return_cnee_name": "",
+    "return_cnee_contact": "",
+    "return_address": "",
+    "return_address_province": "",
+    "return_address_city": "",
+    "return_address_brgy": "",
+    "return_address_street": "",
+    "return_address_area_code": "",
+    "return_address_port_code": "",
+    "special_instruction": "test the instruction",
+    "attachment": "attached file",
+    "or_number": "MUL7200001",
+    "or_amount": "112",
+    "or_date": "2022-10-22",
+    "inbounding_hub": "MONOH",
+    "dest_hub": "MONOH",
+    "cod_amount": "0.0",
+    "declared_value": "4000",
+    "mps_flg": "Y",
+    "rc_code": "2209",
+    "rc_parent_name": "Forwarding",
+    "rc_child_name": "Domestic Roro",
+    "encoding_date": "2022-10-26",
+    "item_details": [
+      {
+        "item_no": "1",
+        "item_desc": "mobile",
+        "item_qty": "2",
+        "package_code": "GCR",
+        "commodity_code": "PG",
+        "item_cust_shortname": "2020000370",
+        "block_measurement": "N",
+        "item_weight": "28.00",
+        "item_length": "30.00",
+        "item_width": "30.00",
+        "item_height": "100.00",
+        "weight_vol_kg": "51.43",
+        "weight_vol_cbm": "0.18",
+        "weight_charge_kg": "56.00",
+        "weight_charge_cbm": "56.00"
+      }
+    ]
+  }
+
+
+
+  // AWB CHARGES RESPONSE 
+
+
+  {
+    "batch_id" : "WZPA55231298273",
+    "success_count" : 80,
+    "success_list" : [
+      "AB00001B",
+      "AB00002B"
+    ],
+    "failed_count" : 20,
+    "failed_list" : [
+      "AB00001B",
+      "AB00002B"
+    ],
+    "failed_list" : [
+      {
+        "tracking_no" : "AA000001A",
+        "error_msg" : "Invalid date format"
+      },
+      {
+        "tracking_no" : "AA000002A",
+        "error_msg" : "Invalid date format"
+      },
+      {
+        "tracking_no" : "AA000003A",
+        "error_msg" : "Invalid date format"
+      },
+    ]
+  }
